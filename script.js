623e79b40f13dc57e6d80541326102b978a95818
Alexandre Calaça <alexcalaca@gmail.com>
2022-05-25 13:15:20 -0300

    Use fetch api to get external data

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+generateJoke()
+
+ function generateJoke() {
+     const config = {
+         headers: {
+             Accept: 'application/json',              
+         },
+     }
+
+     fetch('https://icanhazdadjoke.com', config)
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+ }
